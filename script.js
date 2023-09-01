52e49025163e661504b089d378246fcfb2e30967
AditiSanjeeva <143198249+AditiSanjeeva@users.noreply.github.com>
2023-09-02 01:46:18 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed('#element', {
+        strings: ['Java', 'Python', 'JavaScript','C/C++','SQL'],
+        typeSpeed: 50,
+    });
+});
+
